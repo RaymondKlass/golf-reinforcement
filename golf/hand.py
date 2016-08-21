b10a84a8f572113f51146a341c9952cd259f2dd8
Raymond Klass <raymond@klassphoto.com>
2016-08-20 21:12:39 -0400

    refine hand logic

--- golf/hand.py
+++ golf/hand.py
@@ -7,15 +7,15 @@ class Hand(object):
         # with i % 2 == 0 cards in the bottom row
         # and i @ 2 != 0 cards in the top row
         
-        if cards_dealt % 2 != 0:
+        if len(cards_dealt) % 2 != 0:
             print('Number of cards dealt must be divisible by 2')
             raise 
         
         self.cards = cards_dealt
-        self.num_cols = cards_dealt / 2
+        self.num_cols = len(cards_dealt) / 2
         
         # We start off with the bottom cards revealed to us
-        self.self_revealed = [c % 2 == 0 for c in range(self.cards)]
+        self.self_revealed = [c % 2 == 0 for c in range(len(self.cards))]
         self.opp_revealed = [False] * len(self.cards)
         
     
@@ -24,7 +24,7 @@ class Hand(object):
         # Score the current hand according to the rules
         
         # First we should split the hand into pairs (the columns)
-        columns = [(a[i], a[i+1]) for a in range(len(self.cards)) if a % 2 == 0]
+        columns = [(self.cards[a], self.cards[a+1]) for a in range(len(self.cards)) if a % 2 == 0]
         score = 0
         
         for pair in columns:
@@ -39,13 +39,13 @@ class Hand(object):
     def __str__(self):
         # Printable Version of a players hand
         # that reveals everything (all information)
-        return '  '.join('  {}\n {}'.format([c for c in self.cards if c % 2 != 0],
-                                         [c for c in self.cards if c % 2 == 0]))
+        return '  '.join('  {}\n {}'.format([self.cards[i] for i in range(len(self.cards)) if i % 2 == 0],
+                                         [self.cards[i] for i in range(len(self.cards)) if i % 2 != 0]))
                                          
     
     
     def _swap(self, row, col, new_card, source_revealed = False):
         # swap the card by row and column with the newly given card
-        self.cards[(row * self.num_cols) + col] = new_card
-        
-        self.revealed[(row * self.num_cols) + col] = source_revealed
+        self.cards[(col * 2) + row] = new_card
+        self.self_revealed[(col * 2) + row] = True
+        self.opp_revealed[(col * 2) + row] = source_revealed

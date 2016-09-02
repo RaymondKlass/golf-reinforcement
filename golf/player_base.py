856c6b462e27175697e5f5afe340ca44582ca426
Raymond Klass <raymond@klassphoto.com>
2016-09-02 09:21:23 -0400

    more descriptive docstring for base player class

--- golf/player_base.py
+++ golf/player_base.py
@@ -1,23 +1,27 @@
 # Base player class
 
 class Player(object):
-    
+
     def __init__(self, board):
         self.board = board
-    
-    
+
+
     def turn_phase_1(self, state, possible_moves=['face_up_card', 'face_down_card', 'knock']):
-        """ Phase 1 of the turn - needs to choose to 
+        """ Phase 1 of the turn - needs to choose to
             1.  Take card face up
             2.  Take face down card from top of deck
             3.  Knock
+            Args:
+                state: State object that hold the current state from the player's perspective
+                possible_moves: List of Strings that describe the possible moves
+            Return:
+                String representing the chosen move
         """
         raise NotImplementedError
-    
-    
+
+
     def turn_phase_2(self, card, state, possible_moves=['return_to_deck', 'swap']):
         """ If player gets here, then they need to exchange the card with one of their cards
             or place the card on the top of the discard pile if they drew a face down card """
-            
+
         raise NotImplementedError
-        
--- golf/random_player.py
+++ golf/random_player.py
@@ -1,28 +1,29 @@
-# A Random player - who makes their choice randomly at every step 
+# A Random player - who makes their choice randomly at every step
 import random
 from player_base import Player
 
 class RandomPlayer(Player):
-    
+
     def turn_phase_1(self, state, possible_moves=['face_up_card', 'face_down_card', 'knock']):
         """ We're just going to make a random selection at every step """
-        
+
         return random.choice(possible_moves)
-        
-        
+
+
     def turn_phase_2(self, card, state, possible_moves=['return_to_deck', 'swap']):
         """ Again - this will be a random choice """
-        
+
         choice = random.choice(possible_moves)
-        
+
         if choice == 'swap':
             # We need to figure out which card we'll be swapping with
             # we should do this at random too
-            
+
             dims = self.hand.shape()
             rand_row = random.randint(0,dims[0]-1)
             rand_column = random.randint(0, dims[1]-1)
-            self.board.swap(card, rand_row, rand_col)
-        
+
+            return ('swap', rand_row, rand_col,)
+
         else:
-            self.board.return_to_deck(card)
+            return ('return_to_deck',)

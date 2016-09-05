a9f5a236d75a8a87b29a1652c88b129a05249780
Raymond Klass <raymond@klassphoto.com>
2016-09-05 13:02:05 -0400

    clean up some minor things

--- golf/match.py
+++ golf/match.py
@@ -36,25 +36,6 @@ class Match(object):
 
 
 def main(argv):
-
-    '''
-    inputfile = ''
-    outputfile = ''
-    try:
-       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-    except getopt.GetoptError:
-       print 'test.py -i <inputfile> -o <outputfile>'
-       sys.exit(2)
-    for opt, arg in opts:
-       if opt == '-h':
-          print 'test.py -i <inputfile> -o <outputfile>'
-          sys.exit()
-       elif opt in ("-i", "--ifile"):
-          inputfile = arg
-       elif opt in ("-o", "--ofile"):
-          outputfile = arg
-    '''
-
     # Setup some defaults
     num_matches = 1
     player1 = None
@@ -95,7 +76,5 @@ def main(argv):
     match.play_k_matches(num_matches)
 
 
-
 if __name__ == '__main__':
-
     main(sys.argv[1:])

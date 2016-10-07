3c516e6d7e83c512f34dbfe67f569881f42996d4
Raymond Klass <raymond@klassphoto.com>
2016-10-07 09:37:57 -0700

    add basic test for hand

--- golf/unit_tests/test_hand.py
+++ golf/unit_tests/test_hand.py
@@ -1,10 +1,37 @@
+''' Unit Tests for Hand Logic '''
 import unittest2
+from random import shuffle
+
+from hand import Hand
+
 
 class TestHand(unittest2.TestCase):
+    ''' Unit tests for the hand class '''
 
     def setUp(self):
-        # this is where setup code would go
-        pass
+        ''' Bootstrap a deck and a standard hand of cards '''
+        print 'Running the setup'
+        self.deck = range(12) * 4
+        shuffle(self.deck)
+
+
+    def _load_hand(self, num_cols=2, cards_dealt=None):
+        ''' Bootstrap a hand according to preferences passed in
+            Args:
+                num_cols - Number of columns for the hand
+                cards_dealt - Optional array of cards dealt for hand
+        '''
+        print self.deck
+        print num_cols
+        # if the cards to be dealt were not passed in, then we should calculate them
+        # from the standard deck loaded into the object at SetUp
+        if not cards_dealt:
+            cards_dealt = self.deck[:num_cols * 2]
+            self.deck = self.deck[num_cols*2:]
+
+        self.cards_dealt = cards_dealt
+        self.hand = Hand(cards_dealt = cards_dealt)
+
 
     def tearDown(self):
         # Stub for where teardown / cleanuo logic would be stores
@@ -12,4 +39,12 @@ class TestHand(unittest2.TestCase):
 
 
     def test_hand_setup(self):
-        self.assertEqual(1,1)
+        ''' Test some initial state on the standard hand '''
+        self._load_hand()
+
+        self.assertEqual(self.hand.cards, self.cards_dealt)
+        self.assertEqual(2, self.hand.num_cols)
+
+
+
+

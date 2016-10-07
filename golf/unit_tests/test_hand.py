3dc810804391f052bb98827dbca52b7c77b32527
Raymond Klass <raymond@klassphoto.com>
2016-10-07 08:31:14 -0700

    add a stub for testing

---
+++ golf/unit_tests/test_hand.py
@@ -0,0 +1,15 @@
+import unittest2
+
+class TestHand(unittest2.TestCase):
+
+    def setUp(self):
+        # this is where setup code would go
+        pass
+
+    def tearDown(self):
+        # Stub for where teardown / cleanuo logic would be stores
+        pass
+
+
+    def test_hand_setup(self):
+        self.assertEqual(1,1)

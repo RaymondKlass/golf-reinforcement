244cec6299857d3b04d02227b0e50aa1ed4ed1b5
Raymond Klass <raymond@klassphoto.com>
2016-11-12 12:15:51 -0500

    refine test player base, start testing random player

--- golf/unit_tests/test_player/player_test_base.py
+++ golf/unit_tests/test_player/player_test_base.py
@@ -7,29 +7,29 @@ from golf.hand import Hand
 class PlayerTestBase(unittest2.TestCase):
     ''' Base class for building tests for players '''
 
-    def _load_state(self, num_cols=2, self_cards=None, opp_cards=None):
-        ''' load a configurable state which can be used for tests '''
+    def _load_state(self, deck=None , num_cols=2, num_hands=2, cards=[]):
+        ''' load a configurable state which can be used for tests
+            Args:
+                deck: If specified - the deck will be used to deal any hands that
+                      were not explicitly soecified
+                cards: needs to be an array of cards
+        '''
 
         # this is the entire deck - we'll need to remove cards specified above
         # so as to have a valid deck after hands are dealt
-        self.deck = range(13) * 4
+        if deck:
+            self.deck = deck
+        else:
+            self.deck = range(13) * 4
 
+        self.hands = []
         # if the cards to be dealt were not passed in, then we should calculate them
         # from the standard deck loaded into the object at SetUp
-        if self_cards:
-            cards_dealt = self_cards
-        else:
-            cards_dealt = self.deck[:num_cols * 2]
-            self.deck = self.deck[num_cols*2:]
 
-        self.cards_dealt = cards_dealt
-        self.hand = Hand(cards_dealt = cards_dealt)
-
-        if opp_cards:
-            opp_cards = opp_cards
-        else:
-            opp_cards = self.deck[:num_cols * 2]
-            self.deck = self.deck[num_cols*2:]
-
-
-        self.opp_hand = Hand(cards_dealt = opp_cards)
+        for i in range(num_hands):
+            try:
+                self.hands.append(Hand(cards_dealt=cards[i]))
+            except IndexError:
+                # This hand has not been explicitly specified, we'll deal it from the deck
+                self.hands.append(Hand(cards_dealt=self.deck[:num_cols * 2]))
+                self.deck = self.deck[num_cols * 2:]
---
+++ golf/unit_tests/test_player/test_random_player.py
@@ -0,0 +1,21 @@
+""" Test the logic around the random player """
+from golf.unit_tests.test_player.player_test_base import PlayerTestBase
+from golf.players.random_player import RandomPlayer
+
+class TestRandomPlayer(PlayerTestBase):
+    """ Test the Random Player
+        Since this is the base-line player, tests to make sure that the
+        moves it makes are simply legal should be sufficient.
+    """
+
+    def setUp(self):
+        """ Setup the player and hands """
+
+        # Load the players and their state
+        self._load_state()
+
+
+    def test_turn_phase_1(self):
+        """ Test turn Phase 1 - should be one of the possible moves - ['face_up_card', 'face_down_card', 'knock'] """
+
+        pass

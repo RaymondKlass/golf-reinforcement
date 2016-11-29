5ac012d39c1f3e7d02c1aaccd03e5257545dc3b7
Raymond Klass <raymond@klassphoto.com>
2016-11-28 20:23:55 -0500

    remove redundent test

--- golf/unit_tests/test_player/test_bayesball_player.py
+++ golf/unit_tests/test_player/test_bayesball_player.py
@@ -65,31 +65,3 @@ class TestBayesballPlayer(PlayerTestBase):
             diff = self.bayesball_player._calc_score_diff(state, avg=5)
             self.assertEqual(diff, (6+10) - (4+10))
 
-
-    def test_calc_average_card(self):
-        ''' Test calculating the average card remaining in the deck '''
-
-        deck = range(13) * 4
-        self.assertEqual(len(deck), 52)
-
-        # We mark cards 10 and over as 10
-        deck_sum = lambda d: sum([min(10, a) for a in d])
-
-        # There are 300 points in a full deck
-        self.assertEqual(deck_sum(deck), 300)
-
-        # Let's remove some cards
-        hands = [range(4), range(4)]
-        self.assertEqual(sum(sum(hands, [])), 12)
-
-        deck_left = self._deck_minus_cards(cards=sum(hands, []), deck=deck)
-        self._load_hands(cards=hands, deck=deck_left)
-        self._setup_bayesball_player()
-
-        calc_avg = self.bayesball_player._calc_average_card(self._get_state_for_hand(0))
-
-        # only deducting one players-worth of cards, since the player would not know about the
-        # other player's state just yet
-        self.assertEqual(calc_avg, (300 - sum([a for i, a in enumerate(range(4)) if i % 2 == 0]) - sum([min(a, 10) for a in self.deck_up])) / 49.0)
-
-

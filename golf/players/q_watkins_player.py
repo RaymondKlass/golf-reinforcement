7db61a1cb03aaf987e53bd1bdcb8cf58b22af18a
Raymond Klass <raymond@klassphoto.com>
2016-12-29 12:32:14 -0500

    reduce standard discount for q learning player

--- golf/players/q_watkins_player.py
+++ golf/players/q_watkins_player.py
@@ -41,7 +41,7 @@ class QWatkinsPlayer(TrainablePlayer, PlayerUtils):
             self.weights = self._initialize_blank_model()
 
 
-    def setup_trainer(self, checkpoint_dir, learning_rate=0.1, epsilon=.2, discount=0.1):
+    def setup_trainer(self, checkpoint_dir, learning_rate=0.1, epsilon=.2, discount=0.01):
         ''' Setup the training variables
             Args:
                 checkpoint_dir: string -> Directory to store checkpoint files

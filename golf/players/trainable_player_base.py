4aaa66753caf0e667d31ca124c105b736b8e99b5
Raymond Klass <raymond@klassphoto.com>
2017-01-02 10:28:33 -0500

    add methods to trainable player base to support additional parameters and setting player to act optimally for evaluation

--- golf/players/trainable_player_base.py
+++ golf/players/trainable_player_base.py
@@ -8,6 +8,36 @@ class TrainablePlayer(Player):
         return 'Trainable Player'
 
 
+    def __init__(self, *args, **kwargs):
+        super(TrainablePlayer, self).__init__(*args, **kwargs)
+        self._is_trainable = False
+
+
+    @property
+    def is_training(self):
+        return self._is_training
+
+
+    @is_training.setter
+    def is_training(self, value):
+        """ Set the training value - so that a single player can be 'switched' between
+            training and playing optimally.  This will be important for running the
+            player in evaluation mode
+        """
+
+        # Here we're going to simply set the value - but each model should over-ride
+        # this method to archive values etc for when values are changed again.
+        self._is_training = value
+
+
+
+    def valid_trainable_args(self):
+        """ Setup the list of arguments that are acceptable for training -
+        """
+
+        return dict()
+
+
     def setup_trainer(self):
         """ Trainable players are required to implement this one additional method
             to the methods that are already required in Player base class.

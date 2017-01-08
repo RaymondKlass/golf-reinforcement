bf724d660ef832bdcfbb2d2f9c9c8ebbe434cc75
Raymond Klass <raymond@klassphoto.com>
2017-01-08 18:47:00 -0500

    setup stub for q watkins player tests

---
+++ golf/unit_tests/test_player/test_q_watkins_player.py
@@ -0,0 +1,10 @@
+''' Q Watkins player - makes decisions based on q-learning
+'''
+
+from golf.unit_tests.test_player.player_test_base import PlayerTestBase
+from golf.players.q_watkins_player import QWatkinsPlayer
+
+class TestQWatkinsPlayer(PlayerTestBase):
+    ''' Test the policies for the Q Watkins player '''
+
+    pass

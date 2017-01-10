9ed1bb0751bada68f2b3593655cbff5ffcfcd8d7
Raymond Klass <raymond@klassphoto.com>
2017-01-09 19:55:59 -0500

    add basic test to confirm init of trainable player

--- golf/unit_tests/test_trainer.py
+++ golf/unit_tests/test_trainer.py
@@ -25,7 +25,7 @@ class TestTrainer(unittest2.TestCase):
     ''' test the train module which controls multi-hole and multi-game matches '''
 
 
-    def setup_players(self, trainable_index=0, num_players=2):
+    def _setup_players(self, trainable_index=0, num_players=2):
         """ Setup players - with the option to make them trainable """
 
         self.players = []
@@ -36,3 +36,26 @@ class TestTrainer(unittest2.TestCase):
                 player.is_trainable = True
 
             self.players.append(player)
+
+
+    def test_creation_params(self):
+        """ Test the initialization of params for the trainer class """
+
+        trainable_player_index = 1
+        self._setup_players(trainable_index=trainable_player_index)
+        trainer = Trainer(self.players[0],
+                          self.players[1],
+                          trainable_player='player2',
+                          checkpoint_epochs=100)
+
+        for i in range(2):
+            self.assertEqual(trainer.players[i], self.players[i])
+
+        self.assertEqual(trainer.total_holes, 9)
+        self.assertEqual(trainer.trainable_player, trainable_player_index)
+        self.assertEqual(trainer.checkpoint_epochs, 100)
+
+
+
+
+

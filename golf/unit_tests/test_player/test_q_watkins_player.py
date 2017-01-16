5902273b78f1e743a96e9209960c93dc3db67193
Raymond Klass <raymond@klassphoto.com>
2017-01-16 14:39:59 -0500

    basic test for q watkins player

--- golf/unit_tests/test_player/test_q_watkins_player.py
+++ golf/unit_tests/test_player/test_q_watkins_player.py
@@ -1,10 +1,17 @@
 ''' Q Watkins player - makes decisions based on q-learning
 '''
-
+import numpy as np
 from golf.unit_tests.test_player.player_test_base import PlayerTestBase
 from golf.players.q_watkins_player import QWatkinsPlayer
 
 class TestQWatkinsPlayer(PlayerTestBase):
     ''' Test the policies for the Q Watkins player '''
 
-    pass
+    def setUp(self):
+        self.q_watkins = QWatkinsPlayer()
+
+
+    def test_player_name(self):
+        ''' Basic test for setup '''
+
+        self.assertEqual(str(self.q_watkins).lower(), 'Q Watkins Player'.lower())

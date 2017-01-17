fd5dc391c04083cccaa3e2eea23619494b60ce0c
Raymond Klass <raymond@klassphoto.com>
2017-01-17 08:41:29 -0500

    add test for setting up q watkins player

--- golf/unit_tests/test_player/test_q_watkins_player.py
+++ golf/unit_tests/test_player/test_q_watkins_player.py
@@ -15,3 +15,21 @@ class TestQWatkinsPlayer(PlayerTestBase):
         ''' Basic test for setup '''
 
         self.assertEqual(str(self.q_watkins).lower(), 'Q Watkins Player'.lower())
+
+
+    def test_setup_trainer(self):
+        # test setting up a basic trainer
+
+        # def setup_trainer(self, checkpoint_dir, learning_rate=0.00001, epsilon=0.2, discount=0.7, *args, **kwargs):
+        self.assertEqual(self.q_watkins.is_trainable, False)
+
+        self.q_watkins.setup_trainer(checkpoint_dir='my_checkpoint_dir',
+                                     learning_rate=0.001,
+                                     epsilon=0.25,
+                                     discount=0.8)
+
+        self.assertEqual(self.q_watkins.is_trainable, True)
+        self.assertEqual(self.q_watkins.checkpoint_dir, 'my_checkpoint_dir')
+        self.assertEqual(self.q_watkins.learning_rate, 0.001)
+        self.assertEqual(self.q_watkins.epsilon, 0.25)
+        self.assertEqual(self.q_watkins.discount, 0.8)
